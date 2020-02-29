9faf93976b892d124e4135a99c5c3e965c550b37
Jafer <jafer.daltro@gmail.com>
2020-02-29 20:02:00 -0300

    enviando projeto calculadora sem logica concluida

---
+++ lib/models/memory.dart
@@ -0,0 +1,20 @@
+class Memory {
+  String _value = '0';
+
+  void applyCommand(String command) {
+    if (command == 'AC') {
+      _allClear();
+    } else {
+    
+      _value += command;
+    }
+  }
+
+  void _allClear() {
+    _value = '0';
+  }
+
+  String get value {
+    return _value;
+  }
+}
